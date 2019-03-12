8709ecf4bbca9586119461da1ef52e9570878215
daaef <realafe21@gmail.com>
2019-03-12 14:42:25 +0100

    added responsiveness to design and toggle button

---
+++ index.cpp
@@ -0,0 +1,10 @@
+if(studentGrade >= 70){
+    cout << "You got an A";
+    cout << "Keep it up";
+}else if(studentGrade >= 60 && studentGrade < 70){
+    cout << "You got a B";
+    cout << "Nice one";
+}else if(studentGrade >= 50 && studentGrade < 60){
+    cout << "You got a C";
+    cout << "Try Harder";
+}
---
+++ js/script.js
@@ -0,0 +1,5 @@
+function toggleNav(){
+  var dropdownParent = document.querySelector('.nav');
+  dropdownParent.classList.toggle('open');
+  console.log(dropdownParent.classList);
+}
